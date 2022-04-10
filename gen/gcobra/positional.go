3b74b0a240c4f35876ed668ebc22b69a55c707d5
maxlandon <maximelandon@gmail.com>
2022-04-10 10:49:17 -0400

    following the last commit, remove old useless code

--- gen/gcobra/positional.go
+++ gen/gcobra/positional.go
@@ -1,13 +1,11 @@
 package gcobra
 
 import (
-	"fmt"
 	"reflect"
 	"strings"
 
 	"github.com/spf13/cobra"
 
-	"github.com/octago/sflags/internal/convert"
 	"github.com/octago/sflags/internal/positional"
 	"github.com/octago/sflags/internal/tag"
 )
@@ -19,116 +17,36 @@ func positionals(cmd *cobra.Command, stag tag.MultiTag, val reflect.Value) (bool
 		return false, nil
 	}
 
-	req, _ := stag.Get("required") // this is written on the struct, applies to all
-	reqAll := len(req) != 0        // Each field will count as one required minimum
-	doneReq := 0                   // Total count of required arguments
-
 	// Scan all the fields on the struct and build the list of arguments
 	// with their own requirements, and references to their values.
-	positionals, reqTotal, err := positional.Scan(val, reqAll)
-	if err != nil {
+	// Return a type storing all the fields, references, and with the
+	// tools to manage, parse words and raise any errors related
+	positionals, err := positional.ScanArgs(val, stag)
+	if err != nil || positionals == nil {
 		return true, err
 	}
 
-	// The list of handlers that we will bind to our cobra Args function.
-	handlers := make([]cobra.PositionalArgs, 0, len(positionals))
-
-	// Each argument handler is being fed with some "real-time" values
-	// and requirements, for more precise handling/converting, etc.
-	for _, arg := range positionals {
-		// Adjust the number of required arguments AFTER this one,
-		// so that the argument can exit correctly if we have only
-		// enough words to feed the remaining positional slots.
-		reqTotal -= arg.Minimum
-
-		// Make a custom handler for this positional to throw an error.
-		errorHandler := positionalErrorHandler(positionals)
-
-		// And build the handler (parser) function itself
-		argumentHandler := positionalHandler(arg, &doneReq, reqTotal, errorHandler)
-		handlers = append(handlers, argumentHandler)
-	}
-
 	// Finally, assemble all the parsers into our cobra Args function.
-	cmd.Args = cobra.MatchAll(handlers...)
-
-	return true, nil
-}
-
-// positionalHandler builds a function used to parse and validate
-// none/one/more of the positional arguments passed by a cobra.Command.
-func positionalHandler(arg *positional.Arg, start *int, needed int, err positionalErrHandler) cobra.PositionalArgs {
-	handler := func(cmd *cobra.Command, args []string) error {
-		// The stack of words that we will exhaust, and
-		// the number of words we have parsed so far.
-		stack := positional.MakeWords(args, start, needed)
+	cmd.Args = func(cmd *cobra.Command, args []string) error {
+		// Apply the words on the all/some of the positional fields,
+		// returning any words that have not been parsed in fields,
+		// and an error if one of the positionals has failed.
+		retargs, err := positionals.Parse(args)
 
 		// Once we have consumed the words we wanted, we update the
 		// command's return (non-consummed) arguments, to be passed
 		// later to the Execute(args []string) implementation.
-		defer setRemainingArgs(cmd, stack)
+		defer setRemainingArgs(cmd, retargs)
 
-		// If the positional argument is not a list,
-		// simply convert the individual value and return
-		if !stack.Empty() && arg.Value.Type().Kind() != reflect.Slice {
-			return convert.Value(stack.Pop(), arg.Value, arg.Tag)
-		}
-
-		// Else the argument accepts one or more items,
-		// let it exhaust them smartly, breaking/returning
-		// as soon as it has enough of them.
-		return consumePositionals(stack, arg, err)
+		// Directly return the error, which might be non-nil.
+		return err
 	}
 
-	return handler
+	return true, nil
 }
 
-// consumePositionals parses one or more words from the current list of positionals into
-// their struct fields, and returns once its own requirements are satisfied and/or the
-// next positional arguments require words to be passed along.
-func consumePositionals(stack *positional.Words, arg *positional.Arg, err positionalErrHandler) error {
-	// As long as we've got a word, and nothing told us to quit.
-	for !stack.Empty() {
-		// If we have reached the maximum number of args we accept.
-		if (arg.Minimum) > 0 && (arg.Maximum == stack.Parsed()) && arg.Maximum != -1 {
-			return nil
-		}
-
-		// If we have the minimum required, but there are just
-		// enough words for the next arguments, leave them the words.
-		if (stack.Len() == stack.Required()) && stack.Parsed() >= arg.Minimum {
-			return nil
-		}
-
-		// Else if we have not reached our maximum allowed number
-		// of arguments, we are cleared to consume one.
-		next := stack.Pop()
-
-		// Any conversion error is fatal: TODO maybe handle errors
-		if err := convert.Value(next, arg.Value, arg.Tag); err != nil {
-			return err
-		}
-	}
-
-	// If we are still lacking some required words,
-	// but we have exhausted the available ones.
-	if stack.Parsed() < arg.Minimum {
-		return err(*arg, stack)
-	}
-
-	// Or we consumed all the arguments we wanted, without
-	// error, so either exit because we are the last, or go
-	// with the next argument handler we bound.
-	return nil
-}
-
-func isRequired(p *positional.Arg) bool {
-	return (p.Value.Type().Kind() != reflect.Slice && (p.Minimum > 0)) || // Both must be true
-		p.Minimum != -1 || p.Maximum != -1 // And either of these
-}
-
-func setRemainingArgs(cmd *cobra.Command, stack *positional.Words) {
-	if stack == nil || cmd == nil {
+func setRemainingArgs(cmd *cobra.Command, retargs []string) {
+	if len(retargs) == 0 || retargs == nil || cmd == nil {
 		return
 	}
 
@@ -138,7 +56,7 @@ func setRemainingArgs(cmd *cobra.Command, stack *positional.Words) {
 	// Add these arguments in an annotation to be used
 	// in our Run implementation, where we pass just the
 	// unparsed positional arguments to the command Execute(args []string).
-	cmd.Annotations["sflags"] = strings.Join(stack.List(), " ")
+	cmd.Annotations["sflags"] = strings.Join(retargs, " ")
 }
 
 func getRemainingArgs(cmd *cobra.Command) (args []string) {
@@ -151,109 +69,4 @@ func getRemainingArgs(cmd *cobra.Command) (args []string) {
 	}
 
 	return
-}
-
-// positionalErrHandler is a function that builds a complete error message following
-// the failure of an argument handler. We pass it some stuff allowing us to know
-// where we're at, what we've parsed and what not.
-type positionalErrHandler func(current positional.Arg, args *positional.Words) error
-
-// positionalErrorHandler makes a handler to be used in our argument handlers when they fail.
-func positionalErrorHandler(slots []*positional.Arg) positionalErrHandler {
-	handler := func(arg positional.Arg, stack *positional.Words) (err error) {
-		// Compute the list of arguments slots requiring some values
-		if args := getRequiredArgNames(slots, arg); len(args) > 0 {
-			var msg string
-
-			if len(args) == 1 {
-				msg = fmt.Sprintf("the required argument %s was not provided", args[0])
-			} else {
-				msg = fmt.Sprintf("the required arguments %s and %s were not provided",
-					strings.Join(args[:len(args)-1], ", "), args[len(args)-1])
-			}
-
-			return newError(ErrRequired, msg)
-		}
-
-		return nil
-	}
-
-	return handler
-}
-
-// getRequiredArgNames is used by an argument handler to build the correct list of arguments we need.
-func getRequiredArgNames(slots []*positional.Arg, current positional.Arg) (names []string) {
-	// For each of the EXISTING positional argument fields
-	for index, arg := range slots {
-		// Ignore all positional arguments that have not
-		// thrown an error: they have what they need.
-		if index > 0 && index < current.Index {
-			continue
-		}
-
-		// Non required positional won't appear in the message
-		if !isRequired(arg) {
-			continue
-		}
-
-		// If the positional is a single slot, we need its name
-		if arg.Value.Type().Kind() != reflect.Slice {
-			names = append(names, "`"+arg.Name+"`")
-
-			continue
-		}
-
-		// If we have less words to parse than
-		// the minimum required by this argument.
-		if arg.Value.Len() < arg.Minimum {
-			names = append(names, argHasNotEnough(*arg))
-
-			continue
-		}
-
-		// Or the argument only asks for a limited number
-		// of words and we have too many of them.
-		if arg.Maximum != -1 && arg.Value.Len() > arg.Maximum {
-			names = append(names, argHasTooMany(*arg))
-		}
-	}
-
-	return names
-}
-
-// makes a correct sentence when we don't have enough args.
-func argHasNotEnough(arg positional.Arg) string {
-	var arguments string
-
-	if arg.Minimum > 1 {
-		arguments = "arguments, but got only " + fmt.Sprintf("%d", arg.Value.Len())
-	} else {
-		arguments = argumentWordReq
-	}
-
-	argRequired := "`" + arg.Name + " (at least " + fmt.Sprintf("%d",
-		arg.Minimum) + " " + arguments + ")`"
-
-	return argRequired
-}
-
-// makes a correct sentence when we have too much args.
-func argHasTooMany(arg positional.Arg) string {
-	// The argument might be explicitly disabled...
-	if arg.Maximum == 0 {
-		return "`" + arg.Name + " (zero arguments)`"
-	}
-
-	// Or just build the list accordingly.
-	var parsed string
-
-	if arg.Maximum > 1 {
-		parsed = "arguments, but got " + fmt.Sprintf("%d", arg.Value.Len())
-	} else {
-		parsed = argumentWordReq
-	}
-
-	hasTooMany := "`" + arg.Name + " (at most " + fmt.Sprintf("%d", arg.Maximum) + " " + parsed + ")`"
-
-	return hasTooMany
 }
--- gen/gcomp/positional.go
+++ gen/gcomp/positional.go
@@ -2,7 +2,6 @@ package gcomp
 
 import (
 	"reflect"
-	"sync"
 
 	comp "github.com/rsteube/carapace"
 
@@ -17,13 +16,12 @@ func positionals(comps *comp.Carapace, tag tag.MultiTag, val reflect.Value) (boo
 		return false, nil
 	}
 
-	req, _ := tag.Get("required") // this is written on the struct, applies to all
-	reqAll := len(req) != 0       // Each field will count as one required minimum
-
 	// Scan all the fields on the struct and build the list of arguments
 	// with their own requirements, and references to their values.
-	positionals, reqTotal, err := positional.Scan(val, reqAll)
-	if err != nil {
+	// Return a type storing all the fields, references, and with the
+	// tools to manage, parse words and raise any errors related
+	args, err := positional.ScanArgs(val, tag)
+	if err != nil || args == nil {
 		return true, err
 	}
 
@@ -31,12 +29,28 @@ func positionals(comps *comp.Carapace, tag tag.MultiTag, val reflect.Value) (boo
 	// build ones based on struct tag specs.
 	// Put them in a cache of completion callbacks that is accessed
 	// by all positional arguments in order to use their completions.
-	completionCache := getCompleters(positionals, comps)
+	completionCache := getCompleters(args, comps)
+
+	// Make a custom function for consuming the command words,
+	args = positional.WithWordConsumer(args, consumeWith(completionCache))
 
 	// Once we a have a list of positionals, completers for each,
 	// and the number of arguments required, we can build a single
 	// completion handler, similar to our ValidArgs function handler
-	handler := positionalCompleter(positionals, completionCache, reqTotal)
+	handler := func(ctx comp.Context) comp.Action {
+		// Simply call the positionals with our command words.
+		// This function will call each positional with a copy
+		// of the list.
+		// Arguments that don't have enough words to work with
+		// will be ignored. The function blocks until all slots
+		// are done processing their word list.
+		args.ParseConcurrent(ctx.Args)
+
+		// We are done processing some/all of the positional words.
+		// The cache contains all the completions it needs, so we
+		// just unload them into one action to be returned
+		return completionCache.flush(ctx)
+	}
 
 	// And bind this positional completer to our command
 	comps.PositionalAnyCompletion(comp.ActionCallback(handler))
@@ -46,11 +60,11 @@ func positionals(comps *comp.Carapace, tag tag.MultiTag, val reflect.Value) (boo
 
 // getCompleters populates the completers for each positional argument in
 // a list of them, through either implemented methods or struct tag specs.
-func getCompleters(args []*positional.Arg, comps *comp.Carapace) *compCache {
+func getCompleters(args *positional.Args, comps *comp.Carapace) *compCache {
 	// The cache stores all completer functions, to be used later.
 	cache := newCompletionCache()
 
-	for _, arg := range args {
+	for _, arg := range args.Positionals() {
 		// Make parser function, get completer implementations, how many arguments, etc.
 		if completer := typeCompleter(arg.Value); completer != nil {
 			cache.add(arg.Index, completer)
@@ -72,101 +86,62 @@ func getCompleters(args []*positional.Arg, comps *comp.Carapace) *compCache {
 	return cache
 }
 
-// positionalCompleter builds a handler that is used to concurrently analyze
-// the positional words of the command line, and to propose completions with the
-// most educated guess possible: arguments know a lot, and they can even try to
-// actually convert their values to verify if their type/value is good.
-func positionalCompleter(args []*positional.Arg, cache *compCache, needed int) comp.CompletionCallback {
-	handler := func(ctx comp.Context) comp.Action {
-		// All positionals work concurrently
-		compWorkers := &sync.WaitGroup{}
-
-		for _, arg := range args {
-			compWorkers.Add(1)
-
-			// Each argument processes a copy of the words concurently.
-			go func(arg *positional.Arg, wg *sync.WaitGroup) {
-				defer wg.Done()
-
-				// If we don't have enough words for even
-				// considering this positional to be completed.
-				if len(ctx.Args) < arg.StartMin {
-					return
-				}
-
-				// Each positional makes a stack with a copy
-				// of the arguments It knows where to start.
-				words := positional.GetWords(*arg, ctx.Args, needed)
-
-				// The argument will loop over all the argument words
-				if err := consumeWords(arg, words, cache); err != nil {
-					// An error is often unrecoverable, so we should
-					// probably break and populate the completions with
-					// the appropriate error message.
-					// TODO: error message to comps
-					return
-				}
-			}(arg, compWorkers)
+// consumeWith returns a custom handler which will be called on each positional
+// argument, so that it can consume one/more of the positional words and add
+// completions to the cache if needed.
+func consumeWith(comps *compCache) positional.WordConsumer {
+	handler := func(args *positional.Args, arg *positional.Arg) error {
+		// First, pop all the words we KNOW we're not
+		// interested in, which is the number of minimum
+		// required words BEFORE us.
+		for i := 0; i < arg.StartMin; i++ {
+			args.Pop()
 		}
 
-		// Wait until all of our positional arguments have
-		// processed all of the(ir) words, regardless of if
-		// they produced some completion.
-		compWorkers.Wait()
+		// Always complete if we have no maximum
+		if arg.Maximum == -1 {
+			return completeOrIgnore(arg, comps, 0)
+		}
 
-		// We are done processing some/all of the positional words.
-		// The cache contains all the completions it needs, so we
-		// just unload them into one action to be returned
-		return cache.flush(ctx)
+		// If there is a drift between the accumulated words and
+		// the maximum requirements of the PREVIOUS positionals,
+		// we use this drift in order not to pop the words as soon
+		// as we would otherwise do. Useful when more than one positional
+		// arguments have a minimum-maximum range of allowed arguments.
+		drift := arg.StartMax - arg.StartMin
+		actuallyParsed := 0
+
+		// As long as we've got a word, and nothing told us to quit.
+		for !args.Empty() {
+			if drift == 0 {
+				// That we either consider to be parsed by
+				// our current positional slot, we pop an
+				// argument that should be parsed by us.
+				actuallyParsed++
+			} else if drift > 0 {
+				// Or to be left to one of the preceding
+				// positionals, which have still some slots
+				// available for arguments.
+				drift--
+			}
+
+			// Pop the next positional word, as if we would
+			// parse/convert it into our slot at exec time.
+			args.Pop()
+
+			// If we have reached the maximum number
+			// of args we accept, don't complete
+			if arg.Maximum == actuallyParsed {
+				break
+			}
+		}
+
+		// This function makes the final call on whether to
+		// complete for this positional or not.
+		return completeOrIgnore(arg, comps, actuallyParsed)
 	}
 
 	return handler
-}
-
-// consumeWords is called on each positional argument, so that it can consume
-// one/more of the positional words and add completions to the cache if needed.
-func consumeWords(arg *positional.Arg, stack *positional.Words, comps *compCache) error {
-	// Always complete if we have no maximum
-	if arg.Maximum == -1 {
-		return completeOrIgnore(arg, comps, 0)
-	}
-
-	// If there is a drift between the accumulated words and
-	// the maximum requirements of the PREVIOUS positionals,
-	// we use this drift in order not to pop the words as soon
-	// as we would otherwise do. Useful when more than one positional
-	// arguments have a minimum-maximum range of allowed arguments.
-	drift := arg.StartMax - arg.StartMin
-	actuallyParsed := 0
-
-	// As long as we've got a word, and nothing told us to quit.
-	for !stack.Empty() {
-		if drift == 0 {
-			// That we either consider to be parsed by
-			// our current positional slot, we pop an
-			// argument that should be parsed by us.
-			actuallyParsed++
-		} else if drift > 0 {
-			// Or to be left to one of the preceding
-			// positionals, which have still some slots
-			// available for arguments.
-			drift--
-		}
-
-		// Pop the next positional word, as if we would
-		// parse/convert it into our slot at exec time.
-		stack.Pop()
-
-		// If we have reached the maximum number
-		// of args we accept, don't complete
-		if arg.Maximum == actuallyParsed {
-			break
-		}
-	}
-
-	// This function makes the final call on whether to
-	// complete for this positional or not.
-	return completeOrIgnore(arg, comps, actuallyParsed)
 }
 
 // completeOrIgnore finally takes the decision of completing this positional or not.
